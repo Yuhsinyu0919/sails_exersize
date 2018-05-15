a691c889987bfa723ada86c5918416ac90e8355a
Yuhsinyu0919 <smile60250@gmail.com>
2018-05-15 13:26:18 +0800

    create a new page

--- testProject/config/routes.js
+++ testProject/config/routes.js
@@ -17,6 +17,7 @@ module.exports.routes = {
   'GET /':                   { action: 'view-homepage-or-redirect' },
   'GET /welcome':            { action: 'dashboard/view-welcome' },
 
+  'GET /hello':              { view:   'hello'},   //0515 add new page
   'GET /faq':                { view:   'pages/faq' },
   'GET /legal/terms':        { view:   'pages/legal/terms' },
   'GET /legal/privacy':      { view:   'pages/legal/privacy' },
