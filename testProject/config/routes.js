a120a4d494fb30072d7d460a67d9649161a7ef17
Yuhsinyu0919 <smile60250@gmail.com>
2018-05-28 16:48:41 +0800

    put the css file and jquery file

--- testProject/config/routes.js
+++ testProject/config/routes.js
@@ -14,10 +14,10 @@ module.exports.routes = {
   //  ╦ ╦╔═╗╔╗ ╔═╗╔═╗╔═╗╔═╗╔═╗
   //  ║║║║╣ ╠╩╗╠═╝╠═╣║ ╦║╣ ╚═╗
   //  ╚╩╝╚═╝╚═╝╩  ╩ ╩╚═╝╚═╝╚═╝
-  'GET /':                   { action: 'view-homepage-or-redirect' },
+  //'GET /':                   { action: 'view-homepage-or-redirect' },
   'GET /welcome':            { action: 'dashboard/view-welcome' },
 
-  'GET /hello':              { view:   'hello'},   //0515 add new page
+  'GET /':                   { view:   'hello'},   //0515 add new page
   'GET /faq':                { view:   'pages/faq' },
   'GET /legal/terms':        { view:   'pages/legal/terms' },
   'GET /legal/privacy':      { view:   'pages/legal/privacy' },
